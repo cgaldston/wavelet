249bc698dd1a8bcb2db46afffbc074b6608874b8
Caleb Galdston <56410571+cgaldston@users.noreply.github.com>
2022-10-28 11:16:07 -0700

    j

--- NumberServer.java
+++ NumberServer.java
@@ -39,15 +39,4 @@ class ServerHttpHandler implements HttpHandler {
     }
 }
 
-public class Server {
-    public static void start(int port, URLHandler handler) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
-        //create request entrypoint
-        server.createContext("/", new ServerHttpHandler(handler));
-
-        //start the server
-        server.start();
-        System.out.println("Server Started! Visit http://localhost:" + port + " to visit.");
-    }
-}
